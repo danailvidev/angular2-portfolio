dc7c8a9ef41f7826834513d07abf774ae8c19176
danailvidev <danailvidev@gmail.com>
2017-02-18 18:22:31 +0200

    football models

---
+++ src/app/football-api/models/competition.model.ts
@@ -0,0 +1,41 @@
+import { Fixture } from "./fixture.model";
+import { Table } from "./table.model";
+
+export class Competition {
+
+    id: number = null;
+    caption: string = null;
+    league: string = null;
+    year: string = null;
+    currentMatchday: number = null;
+    numberOfMatchdays: number = null;
+    numberOfTeams: number = null;
+    numberOfGames: number = null;
+    lastUpdated: Date = null;
+
+    fixtures: Fixture[] = [];
+    table: Table = null;
+
+    static getName(league: string) {
+        let names = {
+            'PL': 'Premier League',
+            'ELC': 'Championship',
+            'EL1': 'League One',
+            'BL1': '1. Bundesliga',
+            'BL2': '2. Bundesliga',
+            'DED': 'Eredivisie',
+            'FL1': 'Ligue 1',
+            'FL2': 'Ligue 2',
+            'PD': 'Primera Division',
+            'SD': 'Liga Adelante',
+            'SA': 'Serie A',
+            'PPL': 'Primeira Liga',
+        };
+        return names[league] || league;
+    }
+
+    static isAvailable(competition: Competition) {
+        return ['PL', 'ELC', 'EL1', 'BL1', 'BL2', 'DED', 'FL1', 'FL2', 'PD', 'SD', 'SA', 'PPL'].indexOf(competition.league) !== -1;
+    }
+
+}
---
+++ src/app/football-api/models/fixture.model.ts
@@ -0,0 +1,13 @@
+export class Fixture {
+    id: number;
+    awayTeamName: string;
+    date: Date;
+    homeTeamName: string;
+    matchday: number;
+    odds: any;
+    result: {
+        goalsAwayTeam: number;
+        goalsHomeTeam: number;
+    };
+    status: 'SCHEDULED' | 'CANCELED' | 'TIMED' | 'IN_PLAY' | 'POSTPONED' | 'FINISHED';
+}
